d4efdfddbc43f72d75c45145d8067f1d7418e981
clqu <76551996+clqu@users.noreply.github.com>
2022-08-21 10:24:02 +0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,18 @@ import 'styles/globals.css'
 import { Provider as ConnectionProvider } from 'context/connect'
 import { useRouter } from 'next/router'
 import Sidebar from 'components/Sidebar';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return <ConnectionProvider>
+    <Head>
+      <title>Chat App</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+    </Head>
     {router.pathname.includes('rooms') ? (
       <div className="flex">
         <Sidebar />
@@ -17,4 +25,4 @@ function MyApp({ Component, pageProps }) {
   </ConnectionProvider>
 }
 
-export default MyApp
+export default MyApp;
